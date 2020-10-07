1e795f0265f86f2f34349bfcf96d111b62d0f03f
Lori Henderson <lorim95@gmail.com>
2020-10-07 10:31:12 -0400

    Create a favorite car model and an email model

--- insta_user/admin.py
+++ insta_user/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from insta_user.models import InstaUser
+from insta_user.models import InstaUser, FavoriteCar, UserEmail
 
 admin.site.register(InstaUser, UserAdmin)
+admin.site.register(FavoriteCar)
+admin.site.register(UserEmail)
 
 UserAdmin.fieldsets += ('Custom fields set', {'fields': ('bio',)}),
 
