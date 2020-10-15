4314a5c55f13e4727d6a5dbed37f20132d3b3b97
Jessica Benson <pokeyjess72@gmail.com>
2020-10-15 02:45:18 -0400

    Handles error when not uploading photo on post

--- insta_post/forms.py
+++ insta_post/forms.py
@@ -14,17 +14,3 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ["content"]
-
-class EditPostForm(forms.ModelForm):
-    year= forms.ChoiceField(label='year', widget=forms.Select(attrs={"name": "car-years", "id": "car-years"}))
-    make= forms.CharField(label='make', widget=forms.Select(attrs={"name": "car-makes", "id": "car-makes"}))
-    model= forms.ChoiceField(label='model', widget=forms.Select(attrs={"name": "car-models", "id": "car-models"}))
-    class Meta:
-        model = FavoriteCar
-        fields = ["year", "make", "model", "color", "caption", "car_image"]
-
-class EditCommentForm(forms.ModelForm):
-
-    class Meta:
-        model = Comment
-        fields = ["content"]
